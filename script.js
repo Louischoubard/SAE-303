eb0a080377650cc5363068220539a33e3bfee2b2
louischoubard <louis.choubard@gmail.com>
2025-12-16 17:47:59 +0100

    ajout tri par date

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const fileInput = document.getElementById('xls');
-
+const ageSelect = document.getElementById('selectAge');
+const colAge = 1;
+var minAge = 1;
+var maxAge = 13;
+var data;
 fileInput.addEventListener('change', function(e) {
     // Quand le fichier change dans l'input
     const file = e.target.files[0] // => fichier selectionner
@@ -7,11 +11,26 @@ fileInput.addEventListener('change', function(e) {
     
 })
 
+ageSelect.addEventListener('change', () => {
+    var valueAge = ageSelect.value;
+    valueAge = valueAge.split("-");
+    minAge = valueAge[0];
+    maxAge = valueAge[1];
+    exploiterDonnee(data);
+})
+
 function exploiterDonnee(data){
     var output = document.getElementById('result');
+    output.innerHTML ="";
     nombreLigne = data.length
     for(i=0; i<nombreLigne; i++){
         var tr = document.createElement('tr');
+
+        // Ne pas affiche si age < minAge et age > maxAge
+        if(parseInt(data[i][colAge]) < minAge || parseInt(data[i][colAge]) > maxAge){
+            continue;
+        }
+
         var nbCol = data[i].length;
         for(j=0; j<nbCol; j++){
             var td = document.createElement('td');
@@ -27,10 +46,10 @@ function lireFichier(file){
     // Lire le fichier en utilisant FileReader
     const reader = new FileReader();
     reader.onload = function (e) {
-        var data = e.target.result;
+        var dataBrut = e.target.result;
 
         // récupération du classeur sous forme de tableau
-        var workbook = XLSX.read(data, {type: 'array'});
+        var workbook = XLSX.read(dataBrut, {type: 'array'});
         
         // récupération de la première feuille du classeur
         var firstSheet = workbook.Sheets[workbook.SheetNames[0]];
@@ -39,6 +58,7 @@ function lireFichier(file){
         result = XLSX.utils.sheet_to_json(firstSheet, { header: 1 });
         
         // data preview
+        data = result;
         exploiterDonnee(result);
   };
   reader.readAsArrayBuffer(file);
